27ffe89586004249dfdc578f2e24288a71263181
Julian Bolaños Guerrero <90482316+JulianBol@users.noreply.github.com>
2022-11-04 00:36:29 -0600

    Adición de javadocs

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"poop7","l":"Animal"},{"p":"poop7","l":"AnimalAcuatico"},{"p":"poop7","l":"AnimalAereo"},{"p":"poop7","l":"AnimalTerrrestre"},{"p":"poop7","l":"Ballena"},{"p":"poop7","l":"Pajaro"},{"p":"poop7","l":"Perro"},{"p":"poop7","l":"POOP7"}];updateSearchResults();
